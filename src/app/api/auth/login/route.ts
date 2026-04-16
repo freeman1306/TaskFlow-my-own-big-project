67e4bee9f1bc676d1bae18605ed719cae2d2ed68
freeman1306 <zavalkin06@gmail.com>
2026-04-16 16:42:48 +0300

    .

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,14 +1,20 @@
-﻿import { users } from '@/lib/db';
+﻿import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  const user = users.find((user) => user.email === email && user.password === password);
-
-  if (!user) {
+  if (email !== 'test@test.com' || password !== '12345678') {
     return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 });
   }
 
-  return NextResponse.json({ success: true });
+  // create cookie-session here
+  (await cookies()).set('session', ' valid', {
+    httpOnly: true,
+    secure: true,
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+  });
+
+  return NextResponse.json({ ok: true });
 }
