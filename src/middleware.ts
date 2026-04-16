9c1f62cc908bca05081efa5422a0afb30d28b74a
freeman1306 <zavalkin06@gmail.com>
2026-04-16 16:42:34 +0300

    before renaming to proxy

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+﻿import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const session = req.cookies.get('session')?.value;
+
+  const isAuthPage = req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register');
+
+  // if no session then redirect to login page
+  if (!session && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  // if session - no to login
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/projects/:path*', '/tasks/:path*', '/login', '/register'],
+};
